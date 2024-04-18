e4e3cbb8e192967d9a54813ce02a47f4c5481301
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-04-17 18:12:15 -0700

    beginning assignment

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import MoleContainer from './Components/moleContainer';
+import './App.css';
+
+function App() {
+  let [score, setScore] = useState(0);
+
+  const createMoleHill = () => {
+    let hills = []
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer 
+          key={i} // Add the key prop with a unique value
+          setScore={setScore} 
+          score={score} 
+        />
+      );
+    }
+  
+  return (
+    <div>
+      {hills}
+    </div>
+  )
+}
+    return (
+      <div className='App'>
+        <h1>React-a-Mole</h1>
+        <h2>Score: {score}</h2>
+        {createMoleHill()}
+      </div>
+    )
+}
+
+export default App;
---
+++ src/Components/moleContainer.js
@@ -0,0 +1,12 @@
+import Mole from './mole';
+
+function MoleContainer(props) {
+    return (
+        <div>
+            <h2> Mole Container</h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default MoleContainer;
